c57b4af8bc5169315a937841b6ff197c59538e82
vsoff <rucamper@mail.ru>
2018-08-28 10:23:55 +0300

    Init commit

---
+++ GooglePlayParser/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GooglePlayParser.Startup))]
+namespace GooglePlayParser
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
